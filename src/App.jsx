07018117d7328ff7229ebf3c37c0e7e7b38f052d
MauroTuzzolino <tuzzolinomauro.prog05@gmail.com>
2025-05-13 16:21:52 +0200

    Miglioramento della funzione di creazione delle card

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import MyNav from "./components/MyNav";
 import MyFooter from "./components/MyFooter";
 import Welcome from "./components/Welcome";
-import AllTheBooks from "./components/AllTheBooks";
+import BookFilter from "./components/BookFilter";
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
       <MyNav />
       <main>
         <Welcome />
-        <AllTheBooks />
+        <BookFilter />
       </main>
       <MyFooter />
     </>
