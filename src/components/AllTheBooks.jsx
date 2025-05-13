05f126318bd70b4b8e3f4b48a3bdf95d80c48d38
MauroTuzzolino <tuzzolinomauro.prog05@gmail.com>
2025-05-13 14:23:02 +0200

    Miglioramento css delle card

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -1,5 +1,6 @@
 import { Card, Container, Row, Col } from "react-bootstrap";
 import books from "../books/fantasy.json";
+import "../App.css";
 
 function AllTheBooks() {
   return (
@@ -7,10 +8,12 @@ function AllTheBooks() {
       <Row>
         {books.map((book) => (
           <Col key={book.asin} xs={12} sm={6} md={4} lg={3} className="mb-4">
-            <Card>
-              <Card.Img variant="top" src={book.img} alt={book.title} />
-              <Card.Body>
-                <Card.Title>{book.title}</Card.Title>
+            <Card className="custom-card">
+              <div className="card-img-container">
+                <Card.Img variant="top" src={book.img} alt={book.title} className="card-img" />
+              </div>
+              <Card.Body className="custom-card-body">
+                <Card.Title className="text-truncate">{book.title}</Card.Title>
                 <Card.Text>Price: ${book.price}</Card.Text>
               </Card.Body>
             </Card>
